b69a3418b8bd5913cf9c53e8669a05575eb2b8b5
okuno <sin.okuno@gmail.com>
2026-07-26 14:09:06 +0900

    コンポーネント構成も定義するように修正

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'product-structure',
-    loadChildren: () =>
-      import('./features/product-structure/product-structure.module').then(
-        (m) => m.ProductStructureModule,
-      ),
-  },
-  { path: '', redirectTo: 'product-structure', pathMatch: 'full' },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
